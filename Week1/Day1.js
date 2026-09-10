a9f9ae8f121df978404d88afb1882a07b44caa3e
Venkatesh-Nugula <venkatesh.nugula@icloud.com>
2026-09-11 02:32:28 +0530

    Week 1 - Day 1 JavaScript basics

---
+++ Week1/Day1.js
@@ -0,0 +1,8 @@
+let Age = 29;
+const Name="Venkatesh";
+const Description="Learning CAPM";
+console.log(Age);
+console.log(Name);
+console.log(Description);
+Age = Age +2;
+console.log(Age +"" + Name +""+ Description);
